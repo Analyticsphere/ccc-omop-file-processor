9a7afb99a69e2466a9ea89f7f355624d6c9bcd31
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-01-01 14:32:47 -0500

    csv to parquet functionality

--- core/endpoints.py
+++ core/endpoints.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
 import core.utils as utils
+import core.constants as constants
 import os
 
 app = Flask(__name__)
 
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
+    utils.logger.info("API status check called")
+    
     return jsonify({
         'status': 'healthy',
         'timestamp': datetime.utcnow().isoformat(),
-        'service': 'ccc-ehr-pipeline'
+        'service': constants.SERVICE_NAME
     }), 200
 
 @app.route('/get_file_list', methods=['GET'])
@@ -18,14 +21,50 @@ def get_files():
     # Get parameters from query string
     bucket = request.args.get('bucket')
     folder = request.args.get('folder')
+
+    utils.logger.info(f"Obtaining files from {folder} folder in {bucket} bucket")
     
     file_list = utils.list_gcs_files(bucket, folder)
 
     return jsonify({
         'status': 'healthy',
         'file_list': file_list,
-        'service': 'ehr-pipeline'
+        'service': constants.SERVICE_NAME
     }), 200
+
+@app.route('/create_artifact_buckets', methods=['GET'])
+def create_artifact_buckets():
+    parent_bucket = request.args.get('parent_bucket')
+
+    utils.logger.info(f"Creating artifact buckets in gs://{parent_bucket}")
+
+    artifact_dir = f"{parent_bucket}/{constants.ArtifactPaths.ARTIFACTS.value}"
+    files_dir = f"{parent_bucket}/{constants.ArtifactPaths.CONVERTED_FILES.value}"
+    report_dir = f"{parent_bucket}/{constants.ArtifactPaths.REPORT.value}"
+    dqd_dir = f"{parent_bucket}/{constants.ArtifactPaths.DQD.value}"
+
+    directories = [artifact_dir, files_dir, report_dir, dqd_dir]
+
+    for directory in directories:
+        utils.create_gcs_directory(directory)
+    
+    return "Directories created successfully", 200
+
+@app.route('/convert_to_parquet', methods=['GET'])
+def convert_to_parquet():
+    file_path: str = request.args.get('file_path')
+
+    try:
+        if file_path.endswith('.csv'):
+            utils.logger.info(f"Converting CSV file {file_path} to Parquet format")
+            utils.csv_to_parquet(file_path)
+        else:
+            utils.logger.info(f"FOR NOW not converting {file_path} to Parquet")
+    
+        return "Converted file to Parquet", 200
+    except:
+        return "Unable to convert Parquet files", 500
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
