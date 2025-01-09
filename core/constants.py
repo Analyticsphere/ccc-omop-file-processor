730f0d3c9c8293ae8cc559c9459eb2fad141fd7b
Jake Peters <jcbptrs@gmail.com>
2025-01-09 11:50:28 -0500

    fixed typo in CDM_SCHEMA_PATH

--- core/constants.py
+++ core/constants.py
@@ -9,7 +9,7 @@ SERVICE_NAME = "omop-file-processor"
 CSV = ".csv"
 PARQUET = ".parquet"
 
-CDM_SCHEMA_PATH = "reference/schema/"
+CDM_SCHEMA_PATH = "reference/schemas/"
 CDM_SCHEMA_FILE_NAME = "schema.json"
 
 class ArtifactPaths(str, Enum):
