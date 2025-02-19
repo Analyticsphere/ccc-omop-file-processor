ee40410f5e663b2e9a7b923bf5faa830319aef2b
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-02-19 15:08:47 -0500

    change read_csv param

--- core/omop_client.py
+++ core/omop_client.py
@@ -12,7 +12,7 @@ def create_optimized_vocab_file(vocab_version: str, vocab_gcs_bucket: str) -> No
 
     # Create the optimized vocabulary file if it doesn't exist
     if not utils.parquet_file_exists(optimized_vocab_path):
-        # Ensure vocab file can be read
+        # Ensure exisiting vocab file can be read
         if not utils.valid_parquet_file(optimized_vocab_path):
             # Ensure vocabulary version actually exists
             if utils.gcs_bucket_exists(vocab_path):
@@ -30,9 +30,9 @@ def create_optimized_vocab_file(vocab_version: str, vocab_gcs_bucket: str) -> No
                                 cr.concept_id_2 AS target_concept_id, -- targets to concept_id's
                                 c2.standard_concept AS target_concept_id_standard, 
                                 c2.domain_id AS target_concept_id_domain
-                            FROM read_csv('gs://{vocab_path}CONCEPT.csv', delimiter='\t') c1
-                            LEFT JOIN read_csv('gs://{vocab_path}CONCEPT_RELATIONSHIP.csv', delimiter='\t') cr on c1.concept_id = cr.concept_id_1
-                            LEFT JOIN read_csv('gs://{vocab_path}CONCEPT.csv', delimiter='\t') c2 on cr.concept_id_2 = c2.concept_id
+                            FROM read_csv('gs://{vocab_path}CONCEPT.csv', delim='\t') c1
+                            LEFT JOIN read_csv('gs://{vocab_path}CONCEPT_RELATIONSHIP.csv', delim='\t') cr on c1.concept_id = cr.concept_id_1
+                            LEFT JOIN read_csv('gs://{vocab_path}CONCEPT.csv', delim='\t') c2 on cr.concept_id_2 = c2.concept_id
                             WHERE IFNULL(cr.relationship_id, '') 
                                 IN ('', {constants.MAPPING_RELATIONSHIPS},{constants.REPLACEMENT_RELATIONSHIPS})
                         ) TO 'gs://{optimized_vocab_path}' {constants.DUCKDB_FORMAT_STRING}
@@ -44,7 +44,6 @@ def create_optimized_vocab_file(vocab_version: str, vocab_gcs_bucket: str) -> No
                     sys.exit(1)
                 finally:
                     utils.close_duckdb_connection(conn, local_db_file)
-
             else:
                 utils.logger.error(f"Vocabulary GCS bucket {vocab_path} not found")
                 sys.exit(1)
