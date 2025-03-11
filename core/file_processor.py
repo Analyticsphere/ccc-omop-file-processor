b8a5af44872c369dc72f006bbdecf34970113e7a
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-11 11:50:58 -0400

    debug logging

--- core/file_processor.py
+++ core/file_processor.py
@@ -326,10 +326,13 @@ def get_normalization_sql_statement(gcs_file_path: str, cdm_version: str) -> str
             # No need to add missing, required rows to row_validity check
 
     coalesce_definitions_sql = ",\n                ".join(coalesce_exprs)
-    # If no columns are in file, replace COALESCE() with statement that always evaulates to false
-    coalesce_definitions_sql = coalesce_definitions_sql.replace('COALESCE()', '1=2')
 
+    utils.logger.warning(f"before replacement row_validity is {row_validity}")
+    if row_validity == "":
+        row_validity = "'faketext'"
+    utils.logger.warning(f"after replacement row_validity is {row_validity}")
     row_validity_sql = ", ".join(row_validity)
+    utils.logger.warning(f"row_validity_sql is {row_validity_sql}")
 
     # Build row_check table with row_hash column
     # Uniquely identify invalid rows using hash generated from concatenting each column
