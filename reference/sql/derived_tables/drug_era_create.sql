d60fbfc10eba10614315f1ffb332554e0254467f
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-13 13:38:30 -0400

    remove casts

--- reference/sql/derived_tables/drug_era_create.sql
+++ reference/sql/derived_tables/drug_era_create.sql
@@ -8,13 +8,13 @@ CREATE OR REPLACE TABLE ctePreDrugTarget AS
         d.drug_exposure_id,
         d.person_id,
         c.concept_id AS ingredient_concept_id,
-        CAST(d.drug_exposure_start_date AS DATE) AS drug_exposure_start_date,
+        d.drug_exposure_start_date AS drug_exposure_start_date,
         d.days_supply AS days_supply,
         COALESCE(
             CASE WHEN d.drug_exposure_end_date IS NOT NULL THEN CAST(d.drug_exposure_end_date AS DATE) END,
-            CASE WHEN TRY_CAST(d.days_supply AS INTEGER) IS NOT NULL AND d.drug_exposure_start_date IS NOT NULL 
-                 THEN CAST(d.drug_exposure_start_date AS DATE) + CAST(d.days_supply AS INTEGER) * INTERVAL '1' DAY END,
-            CAST(d.drug_exposure_start_date AS DATE) + INTERVAL '1' DAY
+            CASE WHEN d.days_supply IS NOT NULL AND d.drug_exposure_start_date IS NOT NULL 
+                 THEN d.drug_exposure_start_date + d.days_supply * INTERVAL '1' DAY END,
+            d.drug_exposure_start_date + INTERVAL '1' DAY
         ) AS drug_exposure_end_date
     FROM read_parquet('@DRUG_EXPOSURE') d
     JOIN read_parquet('@CONCEPT_ANCESTOR') ca ON ca.descendant_concept_id = d.drug_concept_id
@@ -22,4 +22,4 @@ CREATE OR REPLACE TABLE ctePreDrugTarget AS
     WHERE c.vocabulary_id = 'RxNorm'
     AND c.concept_class_id = 'Ingredient'
     AND d.drug_concept_id != 0
-    AND IFNULL(TRY_CAST(d.days_supply AS INTEGER), 0) >= 0;
+    --AND IFNULL(TRY_CAST(d.days_supply AS INTEGER), 0) >= 0;
