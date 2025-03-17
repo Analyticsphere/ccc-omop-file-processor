e6a7805e8829b29b64dcc586d9a0842a43d9df9b
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-17 13:37:21 -0400

    optimized temp tables

--- reference/sql/derived_tables/drug_era_create.sql
+++ reference/sql/derived_tables/drug_era_create.sql
@@ -1,9 +1,35 @@
+CREATE OR REPLACE TEMP TABLE drug_exposure_optimized AS
+    SELECT
+        drug_exposure_id,
+        person_id,
+        drug_concept_id,
+        drug_exposure_start_date,
+        days_supply,
+        drug_exposure_end_date
+    FROM read_parquet('@DRUG_EXPOSURE')
+;
+
+
+CREATE OR REPLACE TEMP TABLE concept_ancestor_optimized AS
+    SELECT * FROM read_parquet('@CONCEPT_ANCESTOR')
+    WHERE descendant_concept_id IN (
+        SELECT DISTINCT drug_concept_id FROM drug_exposure_optimized
+    )
+;
+
+CREATE OR REPLACE TEMP TABLE concept_optimized AS
+    SELECT * FROM read_parquet('@CONCEPT')
+    WHERE concept_id IN (
+        SELECT DISTINCT ancestor_concept_id FROM concept_ancestor_optimized
+    )
+;
+
 CREATE OR REPLACE TEMP TABLE ctePreDrugTarget AS
     SELECT
         d.*
-    FROM read_parquet('@DRUG_EXPOSURE') d
-    INNER JOIN read_parquet('@CONCEPT_ANCESTOR') ca 
+    FROM drug_exposure_optimized d
+    INNER JOIN concept_ancestor_optimized ca 
         ON ca.descendant_concept_id = d.drug_concept_id
-    INNER JOIN read_parquet('@CONCEPT') c 
+    INNER JOIN concept_optimized c 
         ON ca.ancestor_concept_id = c.concept_id
 ;
