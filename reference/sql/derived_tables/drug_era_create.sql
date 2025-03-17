b4906971a89bbe34c2785f9a47755b6d82e15063
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-17 12:44:08 -0400

    joins

--- reference/sql/derived_tables/drug_era_create.sql
+++ reference/sql/derived_tables/drug_era_create.sql
@@ -1,5 +1,9 @@
 CREATE OR REPLACE TEMP TABLE ctePreDrugTarget AS
     SELECT
-        *
-    FROM read_parquet('@DRUG_EXPOSURE')
+        d.*
+    FROM read_parquet('@DRUG_EXPOSURE') d
+    INNER JOIN read_parquet('@CONCEPT_ANCESTOR') ca 
+        ON ca.descendant_concept_id = d.drug_concept_id
+    INNER JOIN read_parquet('@CONCEPT') c 
+        ON ca.ancestor_concept_id = c.concept_id
 ;
