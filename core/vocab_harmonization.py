e52753f49bb385102bd393a3c3f09280e8d85eca
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-24 17:28:19 -0400

    remove overwrite

--- core/vocab_harmonization.py
+++ core/vocab_harmonization.py
@@ -211,7 +211,7 @@ class VocabHarmonizer:
         partition_statement = f"""
             COPY (
                 SELECT * FROM read_parquet('gs://{self.target_parquet_path}*{constants.PARQUET}')
-            ) TO 'gs://{self.target_parquet_path}' (FORMAT PARQUET, PARTITION_BY (target_table), OVERWRITE, COMPRESSION ZSTD);
+            ) TO 'gs://{self.target_parquet_path}' (FORMAT PARQUET, PARTITION_BY (target_table), COMPRESSION ZSTD);
         """
 
         part_no_return = partition_statement.replace('\n','')
