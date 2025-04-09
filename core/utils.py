6b00b0766d6525bf520cccf650430defa88daada
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-04-09 15:26:18 -0400

    small fixes

--- core/utils.py
+++ core/utils.py
@@ -47,9 +47,6 @@ def create_duckdb_connection() -> tuple[duckdb.DuckDBPyConnection, str]:
         # https://duckdb.org/docs/configuration/overview.html#global-configuration-options
         conn.execute(f"SET threads={constants.DUCKDB_THREADS}")
 
-        # Reduce write flush threshold to lower chance of OOM
-        #conn.execute("SET partitioned_write_flush_threshold = 1024")
-
         # Set max size to allow on disk
         # Unneeded when writing to GCS
         conn.execute(f"SET max_temp_directory_size='{constants.DUCKDB_MAX_SIZE}'")
